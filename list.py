2195da323f6b02b064c96357779a40edcd614ad2
AleksandrRin <vashsalut76@gmail.com>
2023-06-06 18:38:22 +0300

    123

---
+++ list.py
@@ -0,0 +1,14 @@
+friends = ['ivan', 'oleg', 'petr', 'nikolai']
+
+print(f'{friends[0].title()} приходи ко мне в гости')
+print(f'{friends[1].title()} приходи ко мне в гости')
+print(f'{friends[2].title()} приходи ко мне в гости')
+print(f'{friends[3].title()} приходи ко мне в гости')
+
+friends[-1] = 'stas'
+print(f'{friends} новые гости')
+
+print(f'{friends[0].title()} обещал прийти в гости')
+print(f'{friends[1].title()} обещал прийти в гости')
+print(f'{friends[2].title()} обещал прийти в гости')
+print(f'{friends[3].title()} обещал прийти в гости')
